a7cd16bd7d72517caac00db4f81b131ccacf3750
Stefan Dimitrijevic <stefanstele95@hotmail.com>
2024-03-23 19:51:02 +0100

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Starting LinVAM setup..."
-mkdir -p ~/.local/lib/LinVAM
+mkdir -p ~/.local/lib/
 mkdir -p ~/.local/share/LinVAM
-cp -r ./LinVAM ~/.local/lib/LinVAM
+cp -r ./LinVAM ~/.local/lib/
 cp ./LinVAM/LinVAM.desktop ~/.local/share/applications/
 cp ./LinVAM/command.list ~/.local/share/LinVAM/
 cp ./LinVAM/profiles.json ~/.local/share/LinVAM/
