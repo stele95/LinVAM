f8bdcd2aa6926ed37c6d4d3eec3e1681bde201f2
Stefan Dimitrijevic <stefanstele95@hotmail.com>
2024-03-24 15:44:38 +0100

    remove copying data files in setup script

--- setup.sh
+++ setup.sh
@@ -2,10 +2,7 @@
 echo "Starting LinVAM setup..."
 mkdir -p ~/.local/lib/
 mkdir -p ~/.local/share/LinVAM
-rsync -av ./LinVAM ~/.local/lib/ --exclude .git
+rsync -av ./LinVAM ~/.local/lib/ --exclude .git --exclude data
 cp ./LinVAM/LinVAM.desktop ~/.local/share/applications/
-cp ./LinVAM/command.list ~/.local/share/LinVAM/
-cp ./LinVAM/profiles.json ~/.local/share/LinVAM/
-rm -rf ~/.local/lib/LinVAM/.git
 rm -rf ./LinVAM
 echo "LinVAM setup finished. You can now start LinVAM from the apps list."
