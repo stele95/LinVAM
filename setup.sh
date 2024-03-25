008f083ec7263a29d19c9888c3415b63de129c25
Stefan Dimitrijevic <stefanstele95@hotmail.com>
2024-03-25 01:01:11 +0100

    update setup script

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ mkdir -p ~/.local/lib/
 mkdir -p ~/.local/share/LinVAM
 rsync -av ./LinVAM ~/.local/lib/ --exclude .git --exclude data --exclude model
 cp ./LinVAM/LinVAM.desktop ~/.local/share/applications/
-cp -r ./model ~/.local/share/LinVAM
+cp -r ./LinVAM/model ~/.local/share/LinVAM
 rm -rf ./LinVAM
 echo "LinVAM setup finished. You can now start LinVAM from the apps list."
