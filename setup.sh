523aa2c36ae043292ba3bb24c7a38ce76a172c9c
Stefan Dimitrijevic <stele95@users.noreply.github.com>
2024-03-25 01:01:44 +0100

    Remove keyboard module (#7)
    * remove keyboard module
    
    * update setup script
    
    * update setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 echo "Starting LinVAM setup..."
 mkdir -p ~/.local/lib/
 mkdir -p ~/.local/share/LinVAM
-rsync -av ./LinVAM ~/.local/lib/ --exclude .git --exclude data
+rsync -av ./LinVAM ~/.local/lib/ --exclude .git --exclude data --exclude model
 cp ./LinVAM/LinVAM.desktop ~/.local/share/applications/
+cp -r ./LinVAM/model ~/.local/share/LinVAM
 rm -rf ./LinVAM
 echo "LinVAM setup finished. You can now start LinVAM from the apps list."
