f9cec05ccd3db5962987eac77144412a8cf90454
Stefan Dimitrijevic <stefanstele95@hotmail.com>
2024-03-26 19:12:47 +0100

    change build-and-install script

--- scripts/build-and-install.sh
+++ scripts/build-and-install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-echo "Starting LinVAM setup..."
-cd ./LinVAM/scripts
 sh ./build.sh
 sudo cp ../build/linvam /usr/local/bin/
 sudo cp ../build/linvamconsole /usr/local/bin/
 sh ./setup.sh
 cp ../src/LinVAM.desktop ~/.local/share/applications/
 sh ./configure-ydotoold.sh
-cd ../..
-rm -rf ./LinVAM
-echo "LinVAM setup finished. You can now start LinVAM from the apps list."
