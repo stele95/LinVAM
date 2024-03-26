c1841dd09f2b7be6c9c29b4815c1853db12cbf69
Stefan Dimitrijevic <stefanstele95@hotmail.com>
2024-03-26 19:06:40 +0100

    move and update scripts

--- scripts/build-and-install.sh
+++ scripts/build-and-install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Starting LinVAM setup..."
-cd ./LinVAM
+cd ./LinVAM/scripts
 sh ./build.sh
-sudo cp ./build/linvam /usr/local/bin/
-sudo cp ./build/linvamconsole /usr/local/bin/
+sudo cp ../build/linvam /usr/local/bin/
+sudo cp ../build/linvamconsole /usr/local/bin/
 sh ./setup.sh
-cp ./LinVAM.desktop ~/.local/share/applications/
+cp ../src/LinVAM.desktop ~/.local/share/applications/
 sh ./configure-ydotoold.sh
-cd ..
+cd ../..
 rm -rf ./LinVAM
 echo "LinVAM setup finished. You can now start LinVAM from the apps list."
--- scripts/build-and-package.sh
+++ scripts/build-and-package.sh
@@ -3,12 +3,12 @@ version=$(<VERSION)
 dir="linvam-v$version"
 mkdir $dir
 sh ./build.sh
-cp build/linvam $dir
-cp build/linvamconsole $dir
+cp ../build/linvam $dir
+cp ../build/linvamconsole $dir
 cp setup.sh $dir
 cp setup-and-install.sh $dir
-cp LinVAM.desktop $dir
+cp ../src/LinVAM.desktop $dir
 cp configure-ydotoold.sh $dir
-cp -r model $dir
+cp -r ../src/model $dir
 zip -r "$dir.zip" $dir
 rm -r $dir
