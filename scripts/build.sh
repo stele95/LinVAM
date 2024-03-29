aedc0a9cbf82518be8a002405f81b1db72f18425
Stefan Dimitrijevic <stefanstele95@hotmail.com>
2024-03-29 09:37:41 +0100

    remove spaces from build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
- python3 -m nuitka \
-     --output-dir=../build \
-     --output-filename=linvam \
-     --follow-import-to=commandeditwnd \
-     --follow-import-to=keyactioneditwnd \
-     --follow-import-to=mouseactioneditwnd \
-     --follow-import-to=pauseactioneditwnd \
-     --follow-import-to=profileeditwnd \
-     --follow-import-to=profileexecutor \
-     --follow-import-to=soundactioneditwnd \
-     --follow-import-to=soundfiles \
-     --follow-import-to=ui_commandeditwnd \
-     --follow-import-to=ui_keyactioneditwnd \
-     --follow-import-to=ui_mainwnd \
-     --follow-import-to=ui_mouseactioneditwnd \
-     --follow-import-to=ui_pauseactioneditwnd \
-     --follow-import-to=ui_profileeditwnd \
-     --follow-import-to=ui_soundactioneditwnd \
-     ../src/linvam.py
+python3 -m nuitka \
+   --output-dir=../build \
+   --output-filename=linvam \
+   --follow-import-to=commandeditwnd \
+   --follow-import-to=keyactioneditwnd \
+   --follow-import-to=mouseactioneditwnd \
+   --follow-import-to=pauseactioneditwnd \
+   --follow-import-to=profileeditwnd \
+   --follow-import-to=profileexecutor \
+   --follow-import-to=soundactioneditwnd \
+   --follow-import-to=soundfiles \
+   --follow-import-to=ui_commandeditwnd \
+   --follow-import-to=ui_keyactioneditwnd \
+   --follow-import-to=ui_mainwnd \
+   --follow-import-to=ui_mouseactioneditwnd \
+   --follow-import-to=ui_pauseactioneditwnd \
+   --follow-import-to=ui_profileeditwnd \
+   --follow-import-to=ui_soundactioneditwnd \
+   ../src/linvam.py
 
- python3 -m nuitka \
-     --output-dir=../build \
-     --output-filename=linvamrun \
-     --follow-import-to=profileexecutor \
-     --follow-import-to=soundfiles \
-     ../src/linvamrun.py
+python3 -m nuitka \
+   --output-dir=../build \
+   --output-filename=linvamrun \
+   --follow-import-to=profileexecutor \
+   --follow-import-to=soundfiles \
+   ../src/linvamrun.py
 
 #python3 -m nuitka \
 #    --output-dir=../build \
