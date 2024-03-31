4a5b0ee4e4da6fe535fa21c6b83df5ad82af0df3
Stefan Dimitrijevic <stefanstele95@hotmail.com>
2024-03-31 22:00:31 +0200

    update scripts for mangohud data display

--- scripts/mangohud-profile.sh
+++ scripts/mangohud-profile.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 linvam=$(ps --no-headers -C linvam -o args,state)
 linvamrun=$(ps --no-headers -C linvamrun -o args,state)
-if [ -n "$linvam" ] || [ -n "$linvamrun" ]
+if [ -n "$linvamrun" ] || [ -f ~/.local/share/LinVAM/.linvamrun ]
+then
+  if [ -f ~/.local/share/LinVAM/.linvamrun ]
+  then
+    cat ~/.local/share/LinVAM/.linvamrun | jq '.profile' | sed "s/\"//g"
+  else
+    echo "ON"
+  fi
+elif [ -n "$linvam" ] || [ -f ~/.local/share/LinVAM/.linvam ]
+then
+  if [ -f ~/.local/share/LinVAM/.linvam ]
     then
-        cat ~/.local/share/LinVAM/config | jq '.profile' | sed "s/\"//g"
+      cat ~/.local/share/LinVAM/.linvam | jq '.profile' | sed "s/\"//g"
     else
-        echo "OFF"
+      echo "ON"
+  fi
+else
+  echo "OFF"
 fi
