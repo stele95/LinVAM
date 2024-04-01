17eb3bb4b850fd4e490b166788bbd069e11b7780
Stefan Dimitrijevic <stefanstele95@hotmail.com>
2024-04-01 12:08:45 +0200

    change install steps

--- scripts/build-and-install.sh
+++ scripts/build-and-install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+set -e
+pip install -r requirements.txt
 sh ./build.sh
 sudo cp linvam /usr/local/bin/
 sudo cp linvamrun /usr/local/bin/
 cp ../src/LinVAM.desktop ~/.local/share/applications/
-sh ./configure-ydotoold.sh
+echo "Set up MangoHud for displaying LinVAM? [y/N]: "
+read MANGOHUD
+if [ "$MANGOHUD" == "y" ] || [ "$MANGOHUD" == "Y" ]
+    then
+        echo "Setting up MangoHud"
+        sh setup-mangohud.sh
+fi
+echo "Installing python requirements with pip..."
--- scripts/setup-mangohud.sh
+++ scripts/setup-mangohud.sh
@@ -1,5 +1,10 @@
 if [ -f ~/.config/MangoHud/MangoHud.conf ]
   then
+    mangohud_linvam=$(cat ~/.config/MangoHud/MangoHud.conf | grep 'custom_text=LinVAM')
+    if [ -n "$mangohud_linvam" ]
+    then
+      echo "Already set up, skipping..."
+    else
       cp mangohud-profile.sh ~/.local/share/LinVAM/
       cp mangohud-language.sh ~/.local/share/LinVAM/
       {
@@ -7,6 +12,7 @@ if [ -f ~/.config/MangoHud/MangoHud.conf ]
         echo "exec=sh ~/.local/share/LinVAM/mangohud-profile.sh";
         echo "exec=sh ~/.local/share/LinVAM/mangohud-language.sh";
       } >> ~/.config/MangoHud/MangoHud.conf
+    fi
   else
       echo "MangoHud.conf file not found in ~/.config/MangoHud/"
 fi
