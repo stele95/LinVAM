2072a04c57948530acd09abcf73c64cd7bdf4ff9
Stefan Dimitrijevic <stefanstele95@hotmail.com>
2024-05-28 14:38:55 +0200

    delete setup python env script since it's no longer used

--- scripts/setup-python-virtual-environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-echo "Setting up python virtual environment at ~/.local/share/LinVAM/venv"
-mkdir -p ~/.local/share/LinVAM
-python -m venv ~/.local/share/LinVAM/venv
-source ~/.local/share/LinVAM/venv/bin/activate
-python -m pip install --upgrade pip wheel
-echo "Installing python requirements with pip..."
-pip install -r ../requirements.txt
