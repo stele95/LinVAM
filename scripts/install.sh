1af527e0ad6c8fcf5b20eaaf05398b0c3505b4b5
Stefan Dimitrijevic <stefanstele95@hotmail.com>
2025-09-14 10:24:49 +0200

    add icon installation to install scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,4 @@ set -e
 source ./configure-uinput-access.sh
 pipx install ..
 sudo cp ../LinVAM.desktop /usr/share/applications/
+sudo cp ../linvam/assets/icons/linvam.svg /usr/share/icons/hicolor/scalable/apps/
--- scripts/steam-deck-install.sh
+++ scripts/steam-deck-install.sh
@@ -4,3 +4,4 @@ source ./steam-deck-configure-uinput-access.sh
 pipx install ..
 mkdir -p ~/.local/share/applications/
 cp ../LinVAM.desktop ~/.local/share/applications/
+cp ../linvam/assets/icons/linvam.svg ~/.local/share/icons/hicolor/scalable/apps/
